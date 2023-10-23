57304bce032b5e5f99e3d8ac1aa06775b41a8a7a
sirharis214 <hariskido214@gmail.com>
2023-10-23 15:00:30 -0400

    installing specific tf version

--- cicd/install-terraform.sh
+++ cicd/install-terraform.sh
@@ -3,14 +3,17 @@
 # fail on error
 set -eu
 
+# Define the Terraform version to install
+TERRAFORM_VERSION="1.5.6"
+
 # install yum-config-manager to manage repo
 sudo yum install -y yum-utils
 
 # add official HashiCorp Linux Repo
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 
-# install tf
-sudo yum -y install terraform
+# Install the specific version of Terraform
+sudo yum -y install "terraform-${TERRAFORM_VERSION}"
 
 # verify
 terraform --version
