409b34387d1318e1fb0fb1f6aab2526d8c040c45
sirharis214 <hariskido214@gmail.com>
2023-10-28 11:01:48 -0400

    test

--- cicd/apply-terraform.sh
+++ cicd/apply-terraform.sh
@@ -4,17 +4,19 @@
 set -eu
 
 # go to root; out of cicd dir
-cd ..
+# cd ..
 
 # init
-terraform init
+# terraform init
 
 # plan and apply
-if terraform apply -auto-approve -no-color -input=false plan.out; then
-    echo "Terraform apply successful."
-else
-    # If terraform apply fails, capture and echo the error message
-    echo "Terraform apply failed. Error details:"
-    terraform show -json plan.out | jq '.planned_values.root_module.resources[] | select(.change.actions[] | contains("create") or contains("update") or contains("delete")).change.after' | jq -r
-    exit 1
-fi
+terraform apply -auto-approve -no-color -input=false plan.out
+
+# if terraform apply -auto-approve -no-color -input=false plan.out; then
+#     echo "Terraform apply successful."
+# else
+#     # If terraform apply fails, capture and echo the error message
+#     echo "Terraform apply failed. Error details:"
+#     terraform show -json plan.out | jq '.planned_values.root_module.resources[] | select(.change.actions[] | contains("create") or contains("update") or contains("delete")).change.after' | jq -r
+#     exit 1
+# fi
