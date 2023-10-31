596bfd644fa827fb3a03e200c0c87600faf985af
sirharis214 <hariskido214@gmail.com>
2023-10-31 15:41:37 -0400

    env var's in buildspec.yml as local vars

--- variables.tf
+++ variables.tf
@@ -1,7 +1,12 @@
-variable "EXTERNAL_ID" {
-  type = string
-}
+# variable "EXTERNAL_ID" {
+#   type = string
+# }
 
-variable "ROLE_ARN" {
-  type = string
+# variable "ROLE_ARN" {
+#   type = string
+# }
+
+locals {
+  EXTERNAL_ID = ""
+  ROLE_ARN = ""
 }
